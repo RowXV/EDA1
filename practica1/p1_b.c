53139856803a356fd13c03c93da0933f8d7e0e54
AyElAldo <72531384+AyElAldo@users.noreply.github.com>
2021-03-15 11:04:00 -0600

    Create p1_b.c
    Creado el 10 de marzo por Brigada 1

---
+++ practica1/p1_b.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+// Creado el 10 de Marzo
+// Creado por Brigada 1
+
+int main(){
+
+    int i,j,matriz[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int *pmatriz[3][3];
+
+     pmatriz[0][0] = matriz;
+        printf("En respuesa al inciso c: La direccion de memoria del matriz es %p.\n\n",pmatriz[0][0]);
+
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+
+
+            pmatriz[i][j] = &matriz[i][j];
+
+        }
+    }
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+
+            printf("Direccion para matriz[%d][%d]: %p\n",i,j,pmatriz[i][j]);
+
+        }
+    }
+
+    return 0;
+}
